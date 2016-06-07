b1253292c1810a27c46ed85455b41fbbac051885
zippies <chenzichaocc@163.com>
2016-06-07 13:54:19 -0400

    update

--- app/main/missionView.py
+++ app/main/missionView.py
@@ -7,7 +7,7 @@ from threading import Thread
 from jinja2 import Template
 from . import url
 from config import Config
-import json,time,redis
+import json,time,redis,requests
 
 
 config = Config()
@@ -62,6 +62,112 @@ def freshStatus(id):
     print(progress)
     progress = json.dumps(progress)
     return progress
+
+
+@url.route("/getErrorChart/<int:id>")
+def getErrorChart(id):
+    progress = statusController.get(id)
+    datas = [
+        ("connectionTimeout",progress.get("e_c",0)),
+        ("responseTimeout",progress.get("e_r",0)),
+        ("unknownError",progress.get("e_u",0)),
+        ("assertionError",progress.get("e_a",0))
+    ]
+
+    datalist = [{"value":data[1],"name":data[0]} for data in datas]
+
+    option = eval(Template(option_template).render(
+        title = "错误分布",
+        typestr = "['connectionTimeout','responseTimeout','unknownError','assertionError']",
+        datalist = datalist
+    ))
+
+    return jsonify(option)
+
+
+@url.route("/getElapseChart/<int:id>")
+def getElapseChart(id):
+    p_list = [0.75,0.8,0.85,0.9,0.95,1]
+    typelist,datalist = [],[]
+    sortedElapseList = redis_conn.sort("%s.elapsed" %id)
+    length = redis_conn.llen("%s.elapsed" %id)
+    if length > 0:
+        for p in p_list:
+            index_count = int(length * p)
+            typename = "< %s 秒" %sortedElapseList[index_count-1].decode()[0:4]
+            typelist.append(typename)
+            datalist.append(index_count)
+
+    option = eval(Template(elapse_template).render(
+        title = "响应时间分布",
+        typelist = json.dumps(typelist),
+        datalist = datalist
+    ))
+
+    return jsonify(option)
+
+
+@url.route("/stopmission/<int:id>",methods=["POST"])
+def stopMission(id):
+    try:
+        mission = Mission.query.filter_by(id=id).first()
+        if mission:
+            redis_conn.set("status-%s" %id,-1)
+    except Exception as e:
+        info["result"] = False
+        info["errorMsg"] = str(e)
+    finally:
+        return jsonify(info)
+
+
+@url.route("/testapi",methods=["GET","POST"])
+def testApi():
+    if request.method == "POST":
+        form = request.form
+        method = form.get("type")
+        url = form.get("url")
+        body = form.get("requestbody",'{}') or '{}'
+        header = form.get("requestheader",'{}') or '{}'
+        conn_timeout = int(form.get("connectionTimeout",5))
+        resp_timeout = int(form.get("responseTimeout",10))
+
+        r = None
+
+        if method.lower() == "post":
+            r = requests.post(url=url,data=eval(body),headers=eval(header),timeout=(conn_timeout,resp_timeout))
+        elif method.lower() == "get":
+            r = requests.get(url=url,params=eval(body),headers=eval(header),timeout=(conn_timeout,resp_timeout))
+        elif method.lower() == "delete":
+            r = requests.delete(url=url)
+        elif method.lower() == "put":
+            r = requests.put(url=url,data=eval(body),headers=eval(header),timeout=(conn_timeout,resp_timeout))
+        else:
+            info["result"] = False
+            info["errorMsg"] = "unsupported method"
+
+        info["message"] = Template(result_template).render(
+            url=url,
+            method=method,
+            req_body=body,
+            req_header=header,
+            resp_body=r.text,
+            resp_header=r.headers
+        )
+
+        return jsonify(info)
+
+result_template = """
+<div style="padding:20px">
+    <label>API:</label> {{ url }} [{{ method }}]<br/>
+    <label>requestHeader:</label> {{ req_header }} <br/>
+    <label>requestBody:</label> {{ req_body }} <br/>
+    <hr>
+    <label>responseHeader:</label> <br/>
+    <textarea class="form-control" style="height:100px">{{ resp_header }}</textarea>
+    <label>responseBody:</label> <br/>
+    <textarea class="form-control" style="height:200px">{{ resp_body }}</textarea>
+</div>
+"""
 
 
 option_template = """{
@@ -158,59 +264,3 @@ elapse_template = """
     "color":["#7EC0EE"]
 }
 """
-
-
-@url.route("/getErrorChart/<int:id>")
-def getErrorChart(id):
-    progress = statusController.get(id)
-    datas = [
-        ("connectionTimeout",progress.get("e_c",0)),
-        ("responseTimeout",progress.get("e_r",0)),
-        ("unknownError",progress.get("e_u",0)),
-        ("assertionError",progress.get("e_a",0))
-    ]
-
-    datalist = [{"value":data[1],"name":data[0]} for data in datas]
-
-    option = eval(Template(option_template).render(
-        title = "错误分布",
-        typestr = "['connectionTimeout','responseTimeout','unknownError','assertionError']",
-        datalist = datalist
-    ))
-
-    return jsonify(option)
-
-
-@url.route("/getElapseChart/<int:id>")
-def getElapseChart(id):
-    p_list = [0.75,0.8,0.85,0.9,0.95,1]
-    typelist,datalist = [],[]
-    sortedElapseList = redis_conn.sort("%s.elapsed" %id)
-    length = redis_conn.llen("%s.elapsed" %id)
-    if length > 0:
-        for p in p_list:
-            index_count = int(length * p)
-            typename = "< %s 秒" %sortedElapseList[index_count-1].decode()[0:4]
-            typelist.append(typename)
-            datalist.append(index_count)
-
-    option = eval(Template(elapse_template).render(
-        title = "响应时间分布",
-        typelist = json.dumps(typelist),
-        datalist = datalist
-    ))
-
-    return jsonify(option)
-
-
-@url.route("/stopmission/<int:id>",methods=["POST"])
-def stopMission(id):
-    try:
-        mission = Mission.query.filter_by(id=id).first()
-        if mission:
-            redis_conn.set("status-%s" %id,-1)
-    except Exception as e:
-        info["result"] = False
-        info["errorMsg"] = str(e)
-    finally:
-        return jsonify(info)
